57254871c5a0cfaba58d63fa2d64330469f64f77
devrisby <118779687+devrisby@users.noreply.github.com>
2025-08-11 11:14:38 -0400

    completes 1D - ancedote exercises - svelte

---
+++ part1/svelte-anecdotes/src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+    import data from '$lib/anecdotes'
+
+    let selected = $state(0)
+    let votes = $state(new Array(data.length).fill(0))
+
+    const handleNext = () => selected = Math.floor(Math.random() * data.length)
+    const handleVotes = () => votes[selected]++
+</script>
+
+<h2>Ancedote of the Day</h2>
+<p>{data[selected]}</p>
+<p>has {votes[selected]} votes</p>
+<button onclick={handleVotes}>vote</button>
+<button onclick={handleNext}>next anecdote</button>
+
+<h2>Ancedote with most votes</h2>
+<p>{data[votes.indexOf(Math.max(...votes))]}</p>
+<p>has {votes[votes.indexOf(Math.max(...votes))]} votes</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
