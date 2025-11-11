bfccea8a44cd26e1f9679f66869e772ae5e48cc5
rezbee-dev <118779687+devrisby@users.noreply.github.com>
2025-11-11 17:54:50 -0500

    Complete Part2C - Phonebook - React

--- part2/react-phonebook/src/App.jsx
+++ part2/react-phonebook/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
 
-  
+  // Not using .thenables as shown in Part 2C
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:3001/persons")
+      setPersons(response.data)
+    }
+    
+    fetchData()
+  }, [])
 
   return (
     <div>
@@ -34,11 +38,6 @@ function App() {
           persons={persons}
           setPersons={setPersons}
         />
-        {/* <form onSubmit={onSubmit}>
-          <label>name:  <input value={newName} onChange={ e => setNewName(e.target.value)}/></label>
-          <label>phone: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/></label>
-          <div><button type="submit">add</button></div>
-        </form> */}
       </div>
       <div>
         <h2>Numbers</h2>
