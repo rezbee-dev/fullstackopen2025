6710e04feb110f3eb95549d2941440839174b840
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2026-01-15 14:36:44 -0500

    3B-Phonebook-Svelte: handle error for failed data fetch/no backend

--- part3/svelte-phonebook/frontend/src/routes/+page.js
+++ part3/svelte-phonebook/frontend/src/routes/+page.js
@@ -1,10 +1,17 @@
+import { error } from '@sveltejs/kit'
+
 const baseURL = "http://localhost:3001/persons"
 
 export async function load({fetch}) {
-    const res = await fetch(baseURL)
+    try {
+        const res = await fetch(baseURL)
 
-    // load() must return object, available to page via `data` in `$prop()`
-    return {
-        persons: await res.json()
+        // load() must return object, available to page via `data` in `$prop()`
+        return {
+            persons: await res.json()
+        }
+    } catch (err) {
+        console.error("Error: ", err.message)
+        error(500, "Unable to fetch data")
     }
 }
