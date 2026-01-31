ecd7f84011b04dd327c01da3eff1d520bd1aac4c
rezbee-dev <131298724+rezbee-dev@users.noreply.github.com>
2026-01-31 12:32:13 -0500

    3B-PB-SV: save progress

--- part3/svelte-phonebook/backend/app/db.py
+++ part3/svelte-phonebook/backend/app/db.py
@@ -1,16 +1,36 @@
 from typing import List, Optional
-from app.error import MissingRequiredFieldsError
+from app.error import MissingRequiredFieldsError, DuplicateEntryError
+from app.model import PersonModel, PersonRaw
 
-data: List[dict] = []
+data: List[PersonModel] = []
 
-def get_persons() -> List[dict]:
+def get_persons() -> List[PersonModel]:
     return data
 
-def get_person(id: int) -> Optional[dict]:
-    result = [person for person in data if person["id"] == id]
-    return next(result, None)
+def get_person(id: int) -> Optional[PersonModel]:
+    for p in data:
+        if p.id == id:
+            return p
+    return None
 
-def add_person(person):
-    if not person.get("name") or not person.get("number"):
-        raise MissingRequiredFieldsError("Missing name or number")
+def get_person_by_name(name: str) -> Optional[PersonModel]:
+    for p in data:
+        if p.name == name:
+            return p
+        
+    return None
+
+def add_person(person: PersonRaw):
+    # Due to the nature of Pydantic, error handling will be done in API layer
+    # if not person.get("name") or not person.get("number"):
+    #     raise MissingRequiredFieldsError("Missing name or number")
+    
+    # if get_person_by_name(person):
+    #     raise DuplicateEntryError("Name already exists")
+    
+    new_id = data[-1].id + 1
+    # new_entry: PersonModel = {**person, "id": new_id}
+    data.append(PersonModel(id=new_id, name=person.name, number=person.number))
+
+def delete_person(id: int):
     
--- part3/svelte-phonebook/backend/app/model.py
+++ part3/svelte-phonebook/backend/app/model.py
@@ -1,1 +1,9 @@
 from pydantic import BaseModel
+
+# Todo: need to set custom error message when name or number is missing
+class PersonRaw(BaseModel):
+    name: str
+    number: str
+
+class PersonModel(PersonRaw):
+    id: int
